4722f1c92562a2ebc1cbf662d12c4b0a0ae9d9c4
jo-oo <johannakoreo@gmail.com>
2022-12-29 16:09:31 +0100

    set main layout sections for Moodboard

--- src/pages/moodboard/Moodboard.jsx
+++ src/pages/moodboard/Moodboard.jsx
@@ -1,18 +1,31 @@
 import "./moodboard.scss";
 import logo from '../../assets/logos/logo.svg'
+import Sidebar from "../../components/sidebar/Sidebar";
 
 const Moodboard = () => {
     return (
         <div className="Container">
-            <div>
-                <img src={logo} className="logo" alt="Moodboard logo"/>
+
+            <div className="Moodboard">
+                <Sidebar/>
+                <div className="MainMoodboard">
+                    <div className="Left">
+                        <div>
+                            <h2>PLUS</h2>
+                        </div>  
+                    </div>
+                    <div className="Middle">
+                        <h1>Moodboard</h1>
+                        <div>
+                            <img src={logo} className="Logo" alt="Moodboard Logo"/>
+                        </div>
+                    </div>
+                    <div className="Right">
+                        <h2>Notes</h2>
+                    </div>
+                </div>
             </div>
-            <h1>Moodboard</h1>
-            <div>
-                <p>
-                    Let´s gather all your inspiration
-                </p>
-            </div>
+
         </div>
     )
 }
