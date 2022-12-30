a5ccceb0ac4da34e853cf9e84bce0caa03220feb
jo-oo <johannakoreo@gmail.com>
2022-12-30 14:31:53 +0100

    add logo to Sidebar

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,17 +1,23 @@
 import './sidebar.scss'
+import logo from '../../assets/logos/logo.svg'
 
 const Sidebar = () => {
-  return (
-    <div className="Sidebar">
-        <h1>Sidebar</h1>
-        <h2>Plus</h2>
-        <h2>Category 1</h2>
-        <h2>Category 2</h2>
-        <h2>Category 3</h2>
-        <h2>Category 4</h2>
-        <h2>Category 5</h2>
-    </div>
-  )
+    return (
+        <div className="Sidebar">
+            <div className='Top'>
+                <div>
+                    <img src={logo} className="Logo" alt="Sideboard Logo"/>
+                </div>
+            </div>
+            <h1>Sidebar</h1>
+            <h2>Plus</h2>
+            <h2>Category 1</h2>
+            <h2>Category 2</h2>
+            <h2>Category 3</h2>
+            <h2>Category 4</h2>
+            <h2>Category 5</h2>
+        </div>
+    )
 }
 
 export default Sidebar
