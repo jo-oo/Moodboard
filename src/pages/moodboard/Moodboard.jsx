ab11709adc9cd9b514b2f53f8b79067db0acf31a
jo-oo <johannakoreo@gmail.com>
2023-01-03 17:54:42 +0100

    temporarily move getSnapshot() to Moodbard page to render documents data from "images" collection

--- src/pages/moodboard/Moodboard.jsx
+++ src/pages/moodboard/Moodboard.jsx
@@ -3,7 +3,34 @@ import Sidebar from "../../components/sidebar/Sidebar";
 import Notes from "../../components/notes/Notes";
 import { FiPlus } from 'react-icons/fi';
 
+import { useState, useEffect } from 'react'
+import { collection, getDocs } from 'firebase/firestore'
+//db instance from firebase config file:
+import { db } from '../../firebase/config'
+
+
 const Moodboard = () => {
+    const [data, setData] = useState([])
+
+    useEffect(() => {
+		const getSnapshot = async () => {
+			// get reference to collection 'images'
+			const ref = collection(db, 'images')
+			const snapshot = await getDocs(ref)
+
+            const data = snapshot.docs.map(doc => {
+				return {
+					id: doc.id,
+					...doc.data(),
+				}
+			})
+
+			setData(data)
+
+			console.log("here is the snapshot of the collection 'images' ", snapshot)
+		}
+		getSnapshot()
+	}, [])
 
     return (
         <div className="Container">
@@ -12,6 +39,14 @@ const Moodboard = () => {
                 <Sidebar/>
                 <div className="MainMoodboard">
                     <div className="Middle">
+                        <div>
+                            {data.map((image, index) => (
+                                <div key={index}>
+                                    <p>{image.name} "type:" {image.type} "path:" {image.path} </p>
+                                    
+                                </div>
+                            ))}
+                        </div>
                     </div>
                     <div className="Right">
                         <div>
