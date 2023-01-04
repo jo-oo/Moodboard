97cd64add55f3641c937568db34c01ac48f177f8
jo-oo <johannakoreo@gmail.com>
2023-01-04 19:32:27 +0100

    commented and solved problem with exporting props not read properly by recieving component. Renders data from hook now

--- src/hooks/useGetImages.js
+++ src/hooks/useGetImages.js
@@ -1,38 +1,41 @@
 import { useState, useEffect } from 'react'
 import { collection, getDocs } from 'firebase/firestore'
 //db instance from firebase config file:
-import { db } from '../../firebase/config'
+import { db } from '../firebase/config'
 
 const useGetImages = () => {
     const [data, setData] = useState([])
     const [loading, setLoading] = useState(true)
 
     useEffect(() => {
-		const getSnapshot = async () => {
+        //will run once when it loads and never again. Use snapshot to subscride to updates in the db
+            const getSnapshot = async () => {
 			setLoading(true)
 			// get reference to collection 'images'
 			const ref = collection(db, 'images')
 			const snapshot = await getDocs(ref)
 
-            const data = snapshot.docs.map(doc => {
+            //a snapshot.docs is everything visible in the docs
+            const docsData = snapshot.docs.map(doc => {
+                //return new object with: "id", and spread the data fields of the document, meaning the data of the dpc will become attributes on this object
 				return {
 					id: doc.id,
 					...doc.data(),
 				}
-			})
+			}) 
 
-			setData(data)
+			setData(docsData)
             setLoading(false)
-
+            console.log("got this data from the collection 'images' ", data)
 			console.log("here is the snapshot of the collection 'images' ", snapshot)
 		}
 		getSnapshot()
 	}, [])
 
-    return (
+    return {
         data,
         loading
-    )
+	}
 }
 
 export default useGetImages
--- src/pages/moodboard/Moodboard.jsx
+++ src/pages/moodboard/Moodboard.jsx
@@ -2,42 +2,11 @@ import "./moodboard.scss";
 import Sidebar from "../../components/sidebar/Sidebar";
 import Notes from "../../components/notes/Notes";
 import { FiPlus } from 'react-icons/fi';
-
-import { useState, useEffect } from 'react'
-import { collection, getDocs } from 'firebase/firestore'
-//db instance from firebase config file:
-import { db } from '../../firebase/config'
+import useGetImages from "../../hooks/useGetImages";
 
 
 const Moodboard = () => {
-
-    const [data, setData] = useState([])
-    const [loading, setLoading] = useState(true)
-
-    useEffect(() => {
-        //will run once when it loads and never again. Use snapshot to subscride to updates in the db
-            const getSnapshot = async () => {
-			setLoading(true)
-			// get reference to collection 'images'
-			const ref = collection(db, 'images')
-			const snapshot = await getDocs(ref)
-
-            //a snapshot.docs is everything visible in the docs
-            const data = snapshot.docs.map(doc => {
-                //return new object with: "id", and spread the data fields of the document, meaning the data of the dpc will become attributes on this object
-				return {
-					id: doc.id,
-					...doc.data(),
-				}
-			}) 
-
-			setData(data)
-            setLoading(false)
-            console.log("got this data from the collection 'images' ", data)
-			console.log("here is the snapshot of the collection 'images' ", snapshot)
-		}
-		getSnapshot()
-	}, [])
+    const { data: images, loading } = useGetImages()
 
     return (
         <div className="Container">
@@ -48,7 +17,8 @@ const Moodboard = () => {
                     <div className="Middle">
                         {loading && (<p>Loading..</p>) }
                         <div>
-                            {!loading && data.map((image, index) => (
+                            {!loading && 
+                            images.map((image, index) => (
                                 <div key={index}>
                                     <p>{image.name} "type:" {image.type} "path:" {image.path} </p>
                                     
