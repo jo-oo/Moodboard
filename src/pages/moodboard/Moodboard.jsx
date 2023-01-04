dddf765ab34a694e1e3fedebf3cd663dc8545db6
jo-oo <johannakoreo@gmail.com>
2023-01-04 17:27:30 +0100

    add comments to getSnapshot()

--- src/pages/moodboard/Moodboard.jsx
+++ src/pages/moodboard/Moodboard.jsx
@@ -10,23 +10,30 @@ import { db } from '../../firebase/config'
 
 
 const Moodboard = () => {
+
     const [data, setData] = useState([])
+    const [loading, setLoading] = useState(true)
 
     useEffect(() => {
-		const getSnapshot = async () => {
+        //will run once when it loads and never again. Use snapshot to subscride to updates in the db
+            const getSnapshot = async () => {
+			setLoading(true)
 			// get reference to collection 'images'
 			const ref = collection(db, 'images')
 			const snapshot = await getDocs(ref)
 
+            //a snapshot.docs is everything visible in the docs
             const data = snapshot.docs.map(doc => {
+                //return new object with: "id", and spread the data fields of the document, meaning the data of the dpc will become attributes on this object
 				return {
 					id: doc.id,
 					...doc.data(),
 				}
-			})
+			}) 
 
 			setData(data)
-
+            setLoading(false)
+            console.log("got this data from the collection 'images' ", data)
 			console.log("here is the snapshot of the collection 'images' ", snapshot)
 		}
 		getSnapshot()
@@ -39,14 +46,16 @@ const Moodboard = () => {
                 <Sidebar/>
                 <div className="MainMoodboard">
                     <div className="Middle">
+                        {loading && (<p>Loading..</p>) }
                         <div>
-                            {data.map((image, index) => (
+                            {!loading && data.map((image, index) => (
                                 <div key={index}>
                                     <p>{image.name} "type:" {image.type} "path:" {image.path} </p>
                                     
                                 </div>
                             ))}
                         </div>
+                        
                     </div>
                     <div className="Right">
                         <div>
