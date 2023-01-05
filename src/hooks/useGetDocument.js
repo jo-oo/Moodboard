c5a91c264a26b482c880b6af8030f5e1d283f840
jo-oo <johannakoreo@gmail.com>
2023-01-05 17:19:04 +0100

    moved getSnapshot() of doc to outer scoop to export it

--- src/hooks/useGetDocument.js
+++ src/hooks/useGetDocument.js
@@ -3,36 +3,35 @@ import { doc, getDoc } from 'firebase/firestore'
 //db instance from firebase config file:
 import { db } from '../firebase/config'
 
-const useGetDocument = (collection, id) => {
-    const [data, setData] = useState([])
-    const [loading, setLoading] = useState(true)
+const useGetDocument = (col, id) => {
+	const [data, setData] = useState([])
+	const [loading, setLoading] = useState(true)
 
-    useEffect(() => {
-        //will run once when it loads and never again. Use snapshot to subscride to updates in the db
-            const getSnapshot = async () => {
-			setLoading(true)
-			// get reference to document from unspecified collection
-			const ref = doc(db, collection, id)
-			const snapshot = await getDoc(ref)
+    //Use snapshot to subscride to updates in the db
+	const getSnapshotData = async () => {
+		setLoading(true)
+		// get reference to document in unspecified collection col
+		const documentRef = doc(db, col, id)
+		const snapshot = await getDoc(documentRef)
 
-            if (!snapshot.exists()) {
-                setData(false)
-                setLoading(false)
-                return
-            }
-    
-            setData(snapshot.data())
-            setLoading(false)
-
-            console.log("got this document data from the collection 'images' ", data)
-			console.log("here is the snapshot of the document from collection 'images' ", snapshot)
+		if (!snapshot.exists()) {
+			setData(false)
+			setLoading(false)
+			return
 		}
-		getSnapshot()
+
+		setData(snapshot.data())
+		setLoading(false)
+	}
+
+	useEffect(() => {
+		getSnapshotData()
 	}, [])
 
-    return {
-        data,
-        loading
+	return {
+		data,
+		loading,
+		getSnapshotData,
 	}
 }
 
