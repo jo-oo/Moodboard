a925c0c18d100c0903f6642af96eda22da0276f5
jo-oo <johannakoreo@gmail.com>
2023-01-06 16:31:08 +0100

    created component of "Right Side" in Page Layout due to Tailwind css cluttering the code

--- src/pages/user/SignupPage.jsx
+++ src/pages/user/SignupPage.jsx
@@ -1,5 +1,5 @@
 import SignupForm from '../../components/user/SignupForm'
-import logo from '../../assets/logos/logo.svg'
+import RightSide from '../../components/user/RightSide'
 
 const SignupPage = () => {
 	return (
@@ -7,12 +7,7 @@ const SignupPage = () => {
             <div className='w-1/2 h-full flex flex-col'>
                 <SignupForm />
             </div>
-            <div className='relative w-1/2 h-full flex flex-col pr-4 pl-9 pt-10 bg-[#E1E2EC]'>
-                <div className="absolute top-[5%] left-[8%] right-[8%] flex flex-col bg-slate-100 bg-opacity-20 rounded-lg border-dashed">
-                    <h1 className="m-4 p-2 font-semibold text-slate-700">Gather all your inspiration in one place</h1>
-                </div>
-                <img src={logo} alt="Logo" className="h-full"/>
-            </div>
+            <RightSide/>
         </div>
 	)
 }
