9a9d5021316478117cb5cd51fcb1501c90eed57d
jo-oo <johannakoreo@gmail.com>
2023-01-09 11:46:18 +0100

    create LoginForm using Tailwind classes

--- src/components/user/LoginForm.jsx
+++ src/components/user/LoginForm.jsx
@@ -1,9 +1,93 @@
-import React from 'react'
+import logo from '../../assets/logos/logo.svg'
 
 const LoginForm = () => {
-  return (
-    <div>LoginForm</div>
-  )
+    return (
+        <div>
+
+            <div className="nameContainer flex flex-col items-center min-h-screen sm:justify-center p-8">
+                <div className="nameFormBox w-full px-10 py-12 overflow-hidden bg-white shadow-md sm:max-w-md sm:rounded-lg">
+            
+                    <div className="Logo flex flex-col items-center justify-center mt-2">
+                        <div className='w-11'>
+                            <img src={logo} className="Logo" alt="Logo"/>
+                        </div>
+                    </div>
+
+                    <div className="InfoText flex flex-col items-center justify-center mt-1 mb-7">
+                        <h3 className="text-2xl font-semibold mt-2">Welcome back</h3>
+                        <p className='text-base'>Please enter your details</p>
+                    </div>
+
+                    <form>
+                        <div className="mt-4">
+                            <label
+                                htmlFor="email"
+                                className="block text-sm font-medium text-gray-700"
+                            >
+                                Email
+                            </label>
+                            <div className="flex flex-col items-start">
+                                <input
+                                    type="email"
+                                    name="email"
+                                    className="nameInputField block w-full mt-1 p-1.5 border-[#9EB8EB] border-opacity-30  rounded-md 
+                                    border-2 
+                                    leading-tight focus:outline-none focus:shadow-outline
+                                    focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                                />
+                            </div>
+                        </div>
+                        <div className="mt-4">
+                            <label
+                                htmlFor="password"
+                                className="block text-sm font-medium text-gray-700"
+                            >
+                                Password
+                            </label>
+                            <div className="flex flex-col items-start">
+                                <input
+                                    type="password"
+                                    name="password"
+                                    className="nameInputField block w-full mt-1 p-1.5 border-[#9EB8EB] border-opacity-30  rounded-md 
+                                    border-2 
+                                    leading-tight focus:outline-none focus:shadow-outline
+                                    focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                                />
+                            </div>
+                        </div>
+
+                        <div className="flex items-center justify-end mt-8 mb-2">
+                            <a
+                                className="text-sm text-gray-600 underline hover:text-gray-900"
+                                href="#"
+                            >
+                                Forgot password?
+                            </a>
+                            <button
+                                type="submit"
+                                className="inline-flex items-center px-4 py-2 ml-4 text-xs font-semibold tracking-widest text-white uppercase transition duration-150 ease-in-out bg-gray-900 border border-transparent rounded-md active:bg-gray-900 false"
+                            >
+                                Sign In
+                            </button>
+                        </div>
+                        <div className="mt-4">
+
+                            <div className="flex flex-col items-center">
+                            <a
+                                className="text-sm text-gray-600 underline hover:text-gray-900"
+                                href="#"
+                            >
+                                Don´t have an account? Sign up
+                            </a>
+                            </div>
+                        </div>
+                    </form>
+                    
+                </div>
+            </div>
+
+        </div>
+    )
 }
 
 export default LoginForm
