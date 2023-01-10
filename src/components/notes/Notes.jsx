3450dda652f553171b251928f59aa43102ec0505
jo-oo <johannakoreo@gmail.com>
2023-01-10 14:15:03 +0100

    adding a Log Out, to be button, for authenticated users

--- src/components/notes/Notes.jsx
+++ src/components/notes/Notes.jsx
@@ -1,14 +1,20 @@
 import './notes.scss'
 
 const Notes = () => {
-  return (
-    <div className="NotesContainer">
-        <div className="Notepad">
-            Notes
-            lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-            lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
+    return (
+        <div className='RightContainer'>
+            <div className="NotesContainer">
+                <div className="Notepad">
+                    Notes
+                    lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
+                    lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
+                </div>
+                <div className="LogOut">
+                    <p className="">Log Out</p>
+                </div>
+            </div>
         </div>
-    </div>
+    
   )
 }
 
