637fe598a3121984c3ac0065351f4a12ae5615f8
jo-oo <johannakoreo@gmail.com>
2023-01-10 15:08:12 +0100

    listen for authetication state changes, render LogOut Link when user is logged in

--- src/components/notes/Notes.jsx
+++ src/components/notes/Notes.jsx
@@ -1,6 +1,11 @@
 import './notes.scss'
+import { useAuthContext } from '../../contexts/AuthContext' //to temporarily dispaly user
+import { Link } from 'react-router-dom'
 
 const Notes = () => {
+
+    const { currentUser } = useAuthContext()
+
     return (
         <div className='RightContainer'>
             <div className="NotesContainer">
@@ -10,7 +15,21 @@ const Notes = () => {
                     lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
                 </div>
                 <div className="LogOut">
-                    <p className="">Log Out</p>
+        
+                        {
+							currentUser ? (
+								<>
+									{/* User is logged in */}
+                                    <p>You are logged in as {currentUser.email}!</p>
+									<Link as={Link} to="/logout">Logout</Link>
+								</>
+							) : (
+								<>
+									{/* No user is logged in, show nothing. (They won´t get to this route anyways but might as well write this logic.) */}
+                                    <p>My name</p>
+								</>
+							)
+						}
                 </div>
             </div>
         </div>
--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -1,7 +1,8 @@
 
 import { createContext, useContext, useEffect, useState } from 'react'
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from 'firebase/auth'
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, onAuthStateChanged, } from 'firebase/auth'
 import { auth } from '../firebase/config'
+import BeatLoader from "react-spinners/BeatLoader"
 
 const AuthContext = createContext()
 
@@ -12,6 +13,7 @@ const useAuthContext = () => {
 
 const AuthContextProvider = ({ children }) => {
 	const [currentUser, setCurrentUser] = useState(null)
+	const [loading, setLoading] = useState(true)
 
 
     //create user with email, password and name
@@ -27,8 +29,19 @@ const AuthContextProvider = ({ children }) => {
 		return signOut(auth)
 	}
 
+	useEffect(() => {
+		// listen for authentication-state changes of users
+		const unsubscribe = onAuthStateChanged(auth, (user) => {
+			setCurrentUser(user)
+			setLoading(false)
+		})
+
+		return unsubscribe
+	}, [])
+
 	const contextValues = {
 		//here is what the children should be able to use 
+		currentUser,
 		signup,
         login,
         logout
@@ -36,7 +49,15 @@ const AuthContextProvider = ({ children }) => {
 
 	return (
 		<AuthContext.Provider value={contextValues}>{/* detta är de värden vi tillhandahåller våra children */}
-			{children}
+			{/* {children} */}
+			{/* If Loading, show spinner, else, render children */}
+			{loading ? (
+				<div id="initial-loader">
+					<BeatLoader color={"#888"} size={50} />
+				</div>
+			) : (
+				children
+			)}
 		</AuthContext.Provider>
 	)
 }
