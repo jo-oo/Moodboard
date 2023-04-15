1d95bc52daafed67d13fd9524ca724c6a68a3612
jo-oo <johannakoreo@gmail.com>
2023-04-15 19:51:06 +0200

    restyled sidebar, replace scss with Tailwind

--- src/components/notes/Notes.jsx
+++ src/components/notes/Notes.jsx
@@ -1,22 +1,39 @@
-import './notes.scss'
-import UserProfile from '../user/UserProfile'
 
+import UserProfile from '../user/UserProfile'
+// break-words
 
 const Notes = () => {
 
     return (
-        <div className='RightContainer'>
-            <div className="NotesContainer">
-                <div className="Notepad">
-                    Notes
-                    lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-                    lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-                </div>
-                <UserProfile />
+        <>
+            {/* Container for Notes section */}
+            {/* grid h-screen sticky top-0 IS needed for only the middle section to move / scroll */}
+            <div className='grid grid-cols-1 h-screen sticky top-0
+            bg-[#efeff1] px-3 py-12'>
+
+          
+                    {/* Notes*/}
+                    {/* overflow-y-scroll is setting content overflowing in height/vertically to view in scroll instead*/}
+                    <div className="max-h-screen
+                    p-5 bg-white
+              border-[0.5px]  border-[#CFC9C1] rounded-sm break-words overflow-y-scroll">
+                    Här kan du som användare skriva lite anteckningar till din kategori, alltså till den moddboard su är inne på :-)
+                    Här kan du som användare skriva lite anteckningar till din kategori, alltså till den moddboard su är inne på :-)
+                    .-.-.-.- h
+                    kjkjkj
+                    Här kan du som användare skriva lite anteckningar till din kategori, alltså till den moddboard su är inne på :-)
+                    Här kan du som användare skriva lite anteckningar till din kategori, alltså till den moddboard su är inne på :-)
+                    Här kan du som användare skriva lite anteckningar till din kategori, alltså till den moddboard su är inne på :-)
+
+
+
+                    
+                    </div>
+                    <UserProfile />
+           
             </div>
-        </div>
-    
-  )
+        </>
+    )
 }
 
 export default Notes
