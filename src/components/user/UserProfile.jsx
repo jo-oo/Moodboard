b4d3fa7bf588d4d35b50d506904f834cc0b63449
jo-oo <johannakoreo@gmail.com>
2023-04-15 18:31:51 +0200

    toggle profile icon to small icon and links onClick

--- src/components/user/UserProfile.jsx
+++ src/components/user/UserProfile.jsx
@@ -13,48 +13,56 @@ const UserProfile = () => {
       };
     
     return (
-        <div className="self-end text-start">
-                {
-					currentUser ? (
-						<>	
-                        
-                            {/* User is logged in */}
-                            <p>Hello {userName}!</p>  
+        <>
+            {/* Container for Profile section */}
+            <div className="p-2 ">
+                    {
+                        currentUser ? (
+                            <>	
+                                {/* User is logged in */}
+                                <div className="text-center">
+                                    <p>Hello {userName}!</p>
+                                </div>
+            
+                                <div className='grid place-items-center'>
                             
+                                    {show ? (
+                                        <>  
 
-                            <div className='grid place-items-center'>
-                        
-                             <BsPersonCircle size={40} 
-                                onClick={toggle}/>
-                             
-                                {show ? (
-                                    <>  
-
-                                    <div className='grid grid-cols-3 gap-1'>
-                                    
-                                    <BsPersonCircle size={30} onClick={toggle}/>
-                                        <div className="grid col-span-2">
-                                            <Link as={Link} to="/logout">Logout</Link>
-                                            <Link as={Link} to="/update-profile">Update Profile</Link>
-                                        </div>
-                                        </div>
-                                    </>
-                                    ) : (
-                                        <>
+                                        <div className='grid bg-blue-200 grid-cols-3 gap-1'>
+                                        
+                                        <BsPersonCircle 
+                                            size={30} 
+                                            onClick={toggle}
+                                        />
+                                            <div className="grid col-span-2">
+                                                <Link as={Link} to="/logout">Logout</Link>
+                                                <Link as={Link} to="/update-profile">Update Profile</Link>
+                                            </div>
+                                            </div>
                                         </>
-                                    )
-                                }
-                            </div>
+                                        ) : (
+                                            <>
+                                                <BsPersonCircle 
+                                                    size={40} 
+                                                    onClick={toggle}
+                                                />
+                                            </>
+                                        )
+                                    }
+                                </div>
 
-						</>
-					) : (
-						<>
-							{/* No user is logged in, show nothing. (They won´t get to this route anyways but might as well write this logic.) */}
-                            <p>My name</p>
-						</>
-					)
-				}      
-        </div>
+                            </>
+                        ) : (
+                            <>
+                                {/* No user is logged in, show nothing. (They won´t get to this route anyways but might as well write this logic.) */}
+                                <p>My name</p>
+                            </>
+                        )
+                    } 
+            
+            </div>
+        </>  
     )
 }
 
