af746ee689e3eb6cd2ae22222dccb271b59e5204
jo-oo <johannakoreo@gmail.com>
2023-04-18 17:54:43 +0200

    Updating a image category works with Update ME

--- src/components/images/UpdateImageCategoryForm.jsx
+++ src/components/images/UpdateImageCategoryForm.jsx
@@ -2,82 +2,61 @@
 import Button from "../user/Button"
 import { doc, updateDoc } from 'firebase/firestore'
 import { toast } from 'react-toastify'
-import { useRef, useState } from 'react'
+import { useRef, useState, useEffect } from 'react'
 import { db} from '../../firebase/config'
 import useGetImage  from '../../hooks/useGetImage'
 
-/**
- * 
- * This function is not at all finished. Right now, it updates the category field if the image only on the Button "UPDATE ME". But the handleSubmit-function is not working. 
- * 
- * TO DO
- * 
-
- *          Get image.category: 
-
- *          You will need to send {data} as an object through all the components where you now have taken out two values: selectedImageId, and selectedImageUrl. You will instead in Modal take out the parts from image data that you need to use there (map over it) and then in this component you will need to use the .catepory (YOU COULD MAYBE JUST CALL THE GETIMAGE-HOOK WITH THE ID THAT YOU ALREADY HAVE AND DONT DO ALL THIS STEPS)
- * 
- *  * Use GetCategories hook to do a similar check that you did in "Upload Image", where you check for if the category the user typed in already exists. 
- * If category does exist, AND if the category that the image already belongs to (getImage---image.catgeory) arrays length is more than [0]--> Update category field on image
- * If category does exist, AND if the category that the image already belongs to (getImage---image.catgeory) arrays length is = [0]--> Delete category in collection categories, AND Update category field on image 
- *  * If category does NOT exist, (either just abort: print  "catgeory not existing. choose another on"e ---OR --> user could be able to add category in in this stage..
- * 
- * 
- * CHECK CategoriesTestGrid from old bransch, on how to call on and render categories
-*/
-
+//Get categories
+import useViewCategories from "../../hooks/useViewCategories";
 
 const UpdateImageCategoryForm = ( { onImgCatUpdated, selectedImageUrl, selectedImageId, setShowUpdateImageCategoryForm } ) => {
+    const [selectedCategory, setSelectedCategory] = useState('');
 
-	const inputRef = useRef()
 
-    
+            //fetching the categories of that user
+            const categoriesQuery = useViewCategories({ fetchOnlyCurrentUser: true })
+            // console.log("CAT ", categoriesQuery.data)
+
       //Get me the image! from useGetImage-hook 
       //const {getSnapShotData} = useGetImage(selectedImageId)
       //const {data} = useGetImage({ fetchOnlyCurrentUser: true })
-
-
-    console.log("SelectedImage id " + selectedImageId)
-
-
+        console.log("SelectedImage id " + selectedImageId)
+    
+    
     //updates document in collection "images"
     const updateCategory = async () => {
+        console.log('selected Category from dropdown', selectedCategory);
         await updateDoc(doc(db, 'images', selectedImageId), { 
             //contains these values:
         // category:  inputRef.toLowerCase(), 
-            category: "updated category"     
+            category: selectedCategory 
         })
-        console.log("update image ")
-        toast.success("💪🏻 Todo updated!")
-        onImgCatUpdated()
-                            //toast.success("Imagr is updated!")
+   
+        toast.success("Image updated!")
+    }
+       // onImgCatUpdated()
+                    
                             //onImgCatUpdated()//om vi skulle redigera ett formulär igen vill vi att den nya infon visas
                             //resets form
                             //reset()
                             //sends Admin back to Modal View when succesfullt updating
                             //setShowUpdateImageCategoryForm(false)    
+       
+
+
+        const handleSubmit = async (e) => {
+            e.preventDefault()      
+            updateCategory()
+            console.log("Trigga mig")
         }
-
-
-	const handleSubmit = async (e) => {
-        e.preventDefault()
-        console.log('handleSubmit')
-        
-
-		updateCategory()
-    }
   
 	
 
     return (
         <div>
-
-               
-                <div className="nameFormBox w-full px-2 py-2 overflow-hidden bg-white shadow-md sm:max-w-md sm:rounded-md">
-
+            <div className="nameFormBox w-full px-2 py-2 overflow-hidden bg-white shadow-md sm:max-w-md sm:rounded-md">
 
                     <div className="InfoText flex flex-col items-center justify-center mt-1 mb-1">
-                
                         <p className='text-base'>Please choose category</p>
                     </div> 
 
@@ -86,9 +65,10 @@ const UpdateImageCategoryForm = ( { onImgCatUpdated, selectedImageUrl, selectedI
                         <Alert error={error} />
                     )} */}
 
-                    <form onSubmit={handleSubmit}>
-                
-                        <button onClick= {updateCategory}>UPDATE ME</button>
+
+                    {/* UPDATE FORM */}
+                    <form handleSubmit={handleSubmit}>
+                    
                         <div className="mt-1">
                             <label
                                 htmlFor="category"
@@ -96,22 +76,43 @@ const UpdateImageCategoryForm = ( { onImgCatUpdated, selectedImageUrl, selectedI
                             >
                                 Category
                             </label>
-                            <div className="flex flex-col items-start">
-                                <input
-                                    type="text"
-                                    ref={inputRef} 
-                                    required= "A category must be submitted"
-                                    name="category"
-                                    //defaultValue={selectedImageId.category}
-                                    className="nameInputField block w-full mt-1 p-1.5 border-[#9EB8EB] border-opacity-30  rounded-md 
-                                    border-2 
-                                    leading-tight focus:outline-none focus:shadow-outline
-                                    focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-                                />
-                            </div>
+                                <div className="flex flex-col items-start">
+                                    {/* <input
+                                        type="text"
+                                        ref={inputRef} 
+                                        required= "A category must be submitted"
+                                        name="category"
+                                        
+                                        //defaultValue={selectedImageId.category}
+                                        className="nameInputField block w-full mt-1 p-1.5 border-[#9EB8EB] border-opacity-30  rounded-md 
+                                        border-2 
+                                        leading-tight focus:outline-none focus:shadow-outline
+                                        focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                                    />*/}
+                                </div> 
                         </div>
+
+                        {/* Dropdown */}
+                        <div className="grid gap-3 text-center"> 
+                            <select value={selectedCategory} onChange={e => setSelectedCategory(e.target.value)}>
+                                {categoriesQuery.data && categoriesQuery.data.map((category, i) => {
+                                    console.log("Return cat" , category)
+                                    return (
+                                        
+                                            <>
+                                            <option value={category.name}>{category.name}</option>
+                            
+                                            </>    
+                                    )
+                                })}
+                            </select>
+                        </div>
+
+
                         <div className="flex items-center justify-end mt-8 mb-2">
-                            <button type="submit">submit</button>
+                          {/* CURRENTLY delets image from category but does not add it to the other one */}
+                                <button onClick= {updateCategory}>UPDATE ME</button>
+                                <button type="submit">TEST</button>
                                 <Button type="submit" value={`SEND`}/>
                                 <Button type="reset" value={`RESET`}/>
                         </div>
