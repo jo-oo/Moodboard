028b691c54b46fa9ef335169e34f32b5f692fe1c
jo-oo <johannakoreo@gmail.com>
2023-04-20 00:44:58 +0200

    remove and delete Image Page since only used at start

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import { Routes, Route } from 'react-router-dom'
 import { ReactQueryDevtools } from 'react-query/devtools'
 import RequireAuth from './components/user/RequireAuth'
-//import './App.scss'
 import './index.css'
 
 // * Pages * // 
 import Moodboard from './pages/moodboard/Moodboard'
 import NotFound from './pages/NotFound'
-import ImagePage from './pages/ImagePage'
 import SignupPage from './pages/user/SignupPage'
 import LoginPage from './pages/user/LoginPage'
 import LogoutPage from './pages/user/LogoutPage'
@@ -41,12 +39,6 @@ function App() {
 			<Route path="/update-profile" element={
 				<RequireAuth>
 					<UpdateProfilePage />
-				</RequireAuth>
-			} />
-
-            <Route path="/images/:id" element={
-				<RequireAuth>
-					<ImagePage />
 				</RequireAuth>
 			} />
 		</Routes>
