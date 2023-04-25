7e6c632e59f3441efc853f4f53e83f351a5bf44e
jo-oo <johannakoreo@gmail.com>
2023-04-25 21:44:45 +0200

    move function outside rendering and call in rendering

--- src/components/sidebar/Card.jsx
+++ src/components/sidebar/Card.jsx
@@ -2,14 +2,16 @@ import { useAuthContext } from "../../contexts/AuthContext";
 
 const Card = ( { category } )  => {
 
-    const {setSearchParams } = useAuthContext()
+    const { setSearchParams } = useAuthContext()
+   
+    const handleCategoryChoice= () => {
+        setSearchParams({category: category.name})
+    }
 
     return (
         <div className="">
             <button 
-                onClick={() => 
-                setSearchParams({category: category.name})
-                }
+                onClick={handleCategoryChoice}
                 className="focus:outline-none focus:inline-none
                 hover:border-1 hover:border-[#CFC9C1] leading-5 w-full py-3 bg-[#F6F7F7] text-yellow-950 border-[0.5px] border-[#CFC9C1] shadow-sm"
                 >      
